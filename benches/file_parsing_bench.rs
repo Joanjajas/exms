a6e1d59153aadc9167213b0d633162f40f8c88af
Joanjajas <joanbeltranperis@gmail.com>
2023-06-19 00:13:34 +0200

    Criterion benches added

---
+++ benches/file_parsing_bench.rs
@@ -0,0 +1,13 @@
+use criterion::{criterion_group, criterion_main, Criterion};
+use exms::exam::Exam;
+
+fn file_parsing_bench(c: &mut Criterion) {
+    let mut group = c.benchmark_group("FileParsing");
+    for i in ["benches/bench_files/bench.toml", "benches/bench_files/bench.json"].iter() {
+        group.bench_with_input(*i, i, |b, i| b.iter(|| Exam::from_file(i)));
+    }
+    group.finish();
+}
+
+criterion_group!(benches, file_parsing_bench);
+criterion_main!(benches);
